99f4c49fd55645002f6f81e179f937678c951e5f
Jake Soenneker <jake@soenneker.com>
2024-12-20 11:01:00 -0600

    Tests

---
+++ test/Soenneker.Extensions.DateTime.Nullable.Tests/DateTimeNullableExtensionTests.cs
@@ -0,0 +1,13 @@
+using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Extensions.DateTime.Nullable.Tests;
+
+public class DateTimeNullableExtensionTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
